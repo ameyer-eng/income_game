3ddf596542f529299aa849b4656ed324efc13d4d
ameyer-eng <72287053+ameyer-eng@users.noreply.github.com>
2022-07-23 19:32:35 -0500

    changed to array for luck!

--- classes.js
+++ classes.js
@@ -1,28 +1,45 @@
 
-class Person {
-  constructor(name, balance) {
-    this.name = name;
-    this.balance = balance;
-  }
-    buy()
-    {
-        return "blah";
-    }
+// class Person {
+//   constructor(name, balance) {
+//     this.name = name;
+//     this.balance = balance;
+//   }
+//     buy()
+//     {
+//         document.getElementById("stupidParagraph").innerHTML = "Buy  Button Hit " + this.balance.toString();
+//         this.balance = this.balance - 500;
+//     }
 
 
-}
+// }
 
 
-document.getElementById("buy_button").addEventListener("click", Alice.buy());
-
-const Alice = new Person("Alice", 2000);
-
-//console.log()
-//document.getElementById("stupidParagraph").innerHTML = Alice.name + test();
-
-document.getElementById("stupidParagraph").innerHTML = "ASDFKJSADLFJKASLJDKF";
 
 
-function test(){
-    return "test";
-}
+// const Alice = new Person("Alice", 2000);
+// document.getElementById("buy_button").addEventListener("click", Alice.buy());
+
+//Alice.print_balance();
+
+// //console.log()
+// //document.getElementById("stupidParagraph").innerHTML = Alice.name + test();
+
+//document.getElementById("stupidParagraph").innerHTML = "ASDFKJSADLFJKASLJDKF";
+
+
+
+var myItems = ["Car", "Bus", "House"];
+
+var myDebt = 0;
+
+var myBalance = 5000;
+
+
+document.getElementById("Items").innerHTML = myItems.toString();
+document.getElementById("Balance").innerHTML = myBalance.toString();
+document.getElementById("Debt").innerHTML = myDebt.toString();
+
+function Buy(){
+         myBalance = myBalance - 50;
+         document.getElementById("Balance").innerHTML = myBalance.toString();
+ }
