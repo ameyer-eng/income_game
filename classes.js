2c181c958c4e9112ce1a606b2a33614f1006e504
ameyer-eng <72287053+ameyer-eng@users.noreply.github.com>
2022-07-23 19:04:33 -0500

    initial commit and trial
    going to change from classes to array due to time

---
+++ classes.js
@@ -0,0 +1,28 @@
+
+class Person {
+  constructor(name, balance) {
+    this.name = name;
+    this.balance = balance;
+  }
+    buy()
+    {
+        return "blah";
+    }
+
+
+}
+
+
+document.getElementById("buy_button").addEventListener("click", Alice.buy());
+
+const Alice = new Person("Alice", 2000);
+
+//console.log()
+//document.getElementById("stupidParagraph").innerHTML = Alice.name + test();
+
+document.getElementById("stupidParagraph").innerHTML = "ASDFKJSADLFJKASLJDKF";
+
+
+function test(){
+    return "test";
+}
