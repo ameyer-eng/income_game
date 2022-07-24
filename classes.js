cde470ca01bc09acfcc32843d9e85dcf729ec890
ameyer-eng <72287053+ameyer-eng@users.noreply.github.com>
2022-07-23 20:10:31 -0500

    Buying working.  Need to work on selling function

--- classes.js
+++ classes.js
@@ -24,26 +24,38 @@ var myDebt = 0;
 
 var myBalance = 5000;
 
+var current_item_cost = 0;
+
 
 document.getElementById("Items").innerHTML = "ITEMS:" + myItems.toString();
 document.getElementById("Balance").innerHTML = "Money:" + myBalance.toString();
 document.getElementById("Debt").innerHTML = "DEBT" + myDebt.toString();
 
 function Buy(){
-         myBalance = myBalance - 50;
+         myBalance = myBalance - current_item_cost;
          document.getElementById("Balance").innerHTML = myBalance.toString();
+
+         //call the seller function to put a new item for sale (seller should also be a class....)
          Seller();
 
  }
+
+ 
 
 function Seller(){
     var index =  Math.floor(Math.random()*3);
     console.log(index.toString());
     var items = ["Car", "Bus", "House"];
-    var item_cost = [4000, 8000, 1000000000000000];
+    var item_cost = [4000, 8000, 10];
+    var rates = [1.01, 5.5, 25];
+
+    current_item_cost = item_cost[index];
+
+    myItems.push(items[index]);
+    document.getElementById("Items").innerHTML = "ITEMS:" + myItems.toString();
 
     document.getElementById("SALE").innerHTML = "Item " + items[index].toString() + "  is for sale";
-    document.getElementById("SALE_1").innerHTML = "It will cost " + item_cost[index].toString() + "at rate";
+    document.getElementById("SALE_1").innerHTML = "It will cost " + item_cost[index].toString() + " at rate " + rates[index];
     
 
 }
